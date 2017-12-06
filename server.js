cfd4e86ef9ef04d9ab2e5b3935a5b6450e9e575d
bigalex <bigalex@gmx.de>
2017-12-07 00:42:31 +0100

    add favicon and auto reload
     - favicon and inkscape source file
     - reload and watch module for automatic browser refresh

--- server.js
+++ server.js
@@ -1,14 +1,24 @@
 var express = require('express'),
   app = express(),
   port = process.env.PORT || 8888;
+var reload = require('reload');
+var watch = require('watch');
 
 app.use(express.static(__dirname + '/public/html')); // static html
 app.use('/css', express.static(__dirname + '/public/css')); // css
 app.use('/js.3p', express.static(__dirname + '/public/js.3p')); // 3rd party js, all stolen
 app.use('/vendor', express.static(__dirname + '/public/vendor')); // 3rd party libs
 
+reloadServer=reload(app);
+
+
 app.listen(port, function () {
   console.log('...running');
+  watch.watchTree(__dirname + '/public', {interval:1}, function (f, curr, prev) {
+    // Fire server-side reload event
+    reloadServer.reload();
+  });
+
 });
 
 console.log('LEDFLIX server will start on port: ' + port);
